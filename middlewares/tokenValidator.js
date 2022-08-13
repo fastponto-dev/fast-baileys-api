8d5defdd4a843e44b9aac09f34c6941bf227e6ba
TIAGO OLIVEIRA <tiago.oliveira@fastponto.com.br>
2022-08-13 13:40:45 -0300

    set token env

--- middlewares/tokenValidator.js
+++ middlewares/tokenValidator.js
@@ -8,7 +8,7 @@ const validate = (req, res, next) => {
         return response(res, 403, false, 'No token sent!')
     }
 
-    if (req.headers.authorization != process.env.TOKEN) {
+    if (req.headers.authorization != process.env.TOKEN ?? '2d5819c8-1664-4fd7-903b-3c36f84a824f') {
         return response(res, 401, false, 'Unauthorized')
     }
 
